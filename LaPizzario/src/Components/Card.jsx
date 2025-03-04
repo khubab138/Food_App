a3dc19b16a22b2ce9bea8bfcc422bf8e76078c70
Khubab138 <muhammadkhubabsiddiqui@gmail.com>
2025-03-04 10:53:02 -0800

    resolve fetching by using a curlyBraces

--- LaPizzario/src/Components/Card.jsx
+++ LaPizzario/src/Components/Card.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { IoMdStar } from "react-icons/io";
+import FoodItems from "./FoodItems";
 
-const Card = () => {
+
+const Card = ({id, name, price, desc, rating, img}) => {
   return (
     <div className="font-bold w-[250px] p-5 flex flex-col rounded-lg bg-white gap-2" >
       <img
-        src="ttps://img.freepik.com/free-photo/seafood-pizza_74190-5944.jpg?w=996&t=st=1693062328~exp=1693062928~hmac=53fd9ad496580db41c6ca8066510cd89c6b0a0389de8bb6b875a78a1eda09cb5"
+        src={img}
         alt=""
         className="w-auto h-[130px] hover:scale-110 cursor-grab ease-in-out transition-all duration-500"
       />
       <div className="text-sm flex justify-between  ">
-        <h2>Onion Pizza</h2>
-        <span className="text-green-500">Rs 180</span>
+        <h2>{name}</h2>
+        <span className="text-green-500">Rs {price}</span>
       </div>
-      <p className="font-normal text-sm">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vitae, explicabo.</p>
+      <p className="font-normal text-sm">{desc.slice(0, 50)}...</p>
       <div className="flex justify-between ">
         <span className="flex justify-center items-center">
-        <IoMdStar className="mr-1 text-yellow-400" /> 4.5
+        <IoMdStar className="mr-1 text-yellow-400" /> {rating}
         </span>
         <button className="p-1 text-white bg-green-500 hover:bg-green-600 rounded-lg text-sm" >Add to Cart</button>
       </div>
--- LaPizzario/src/Components/FoodItems.jsx
+++ LaPizzario/src/Components/FoodItems.jsx
@@ -2,12 +2,19 @@ import React from "react";
 import Card from "./Card";
 import FoodData from "../assets/FoodData";
 
-const FoodItems = () => {
+const FoodItems = ({id, name, price, desc, rating, img}) => {
   return (
     <div>
       {FoodData.map((food) => {
         return (
           <Card
+          key={food.id}
+          id={food.id}
+          name={food.name}
+          price={food.price}
+          desc={food.desc}
+          rating={food.rating}
+          img={food.img}
           />
         );
       })}
