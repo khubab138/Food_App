c7123a1b3c5c77c3da53c59c51506a8a922bc864
Khubab138 <muhammadkhubabsiddiqui@gmail.com>
2025-03-05 08:28:58 -0800

    in cartSlice forgot to give a suitable token, and it cause a no return functio

--- LaPizzario/src/Redux/slices/CartSlices.jsx
+++ LaPizzario/src/Redux/slices/CartSlices.jsx
@@ -10,9 +10,7 @@ const CartSlice = createSlice({
       state.cart.push(action.payload);
     },
     removeFromCart: (state, action) => {
-    state.cart =  state.cart.filter((item) => {
-        item.id !== action.payload.id;
-      });
+      state.cart = state.cart.filter((item) => item.id !== action.payload.id);
     },
   },
 }); 
