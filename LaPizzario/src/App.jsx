b02f53f091613f1cfd2d7f3e2f41346831bf4b19
Khubab138 <muhammadkhubabsiddiqui@gmail.com>
2025-03-08 10:02:19 -0800

    fixed a bug from checkout

--- LaPizzario/src/App.jsx
+++ LaPizzario/src/App.jsx
@@ -3,13 +3,14 @@ import {BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import Success from './Pages/Success'
 import Error from './Pages/Error'
+import ProtectedRoute from './Components/ProtectedRoute'
 
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<Home/>} />
-      <Route path='/Success' element={<Success/>} />
+      <Route path='/Success' element={<ProtectedRoute element={<Success/>} />} />
       <Route path='/*' element={<Error/>} />
     </Routes>
     
